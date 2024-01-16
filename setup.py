e97b4d4cd1240cf72973b97a0008318464be9605
Rachel Duffin <rachel.g.duffin@gmail.com>
2024-01-16 14:10:56 +0000

    Amend version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="samplesheet_validator",
-    version="1.0.0",
+    version="1.1.0",
     description="Python library for samplesheet validation",
     url="http://github.com/moka-guys/samplesheet_validator",
     author="Rachel Duffin",
